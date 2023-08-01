e1ae06d60b23b2b5440aa7fb0d44ec8158012e4b
Domanrovil Ovalle <84547591+Domanrovil@users.noreply.github.com>
2023-07-31 21:27:41 -0400

    Update DefaultRoles.sql

--- DefaultRoles.sql
+++ DefaultRoles.sql
@@ -1,8 +1,8 @@
-/* Adds default roles to database */
+/* Adds default roles to database Example for Client (NOT FINAL)*/
 
 USE ChurchVMS;
 
- INSERT INTO Roles VALUES (1, 'Applicant', 'Applied and pending approval');
+INSERT INTO Roles VALUES (1, 'Applicant', 'Applied and pending approval');
 INSERT INTO Roles VALUES (2, 'Volunteer', 'Approved volunteer');
 INSERT INTO Roles VALUES (3, 'Team Leader', 'Volunteer who manages a team');
 INSERT INTO Roles VALUES (4, 'Administrator', 'Has full authority to manage system'); 
